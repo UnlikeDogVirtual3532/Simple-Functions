00324c58db53f70f826f1c0d975268e5dca1b735
UnlikeDogVirtual3532 <kobe@roosemeyers.com>
2024-08-17 12:48:16 +0200

    Cleaned Up Code

--- src/main/java/create/convoy/simplefunctions/Playercommands.java
+++ src/main/java/create/convoy/simplefunctions/Playercommands.java
@@ -5,6 +5,7 @@ import com.mojang.brigadier.CommandDispatcher;
 import com.mojang.brigadier.arguments.StringArgumentType;
 import com.mojang.brigadier.suggestion.SuggestionProvider;
 
+import create.convoy.simplefunctions.commands.CallCommand;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
 import net.minecraft.server.command.CommandManager;
@@ -41,31 +42,6 @@ public class Playercommands implements ModInitializer {
                     return Command.SINGLE_SUCCESS;
                 })
                 .then(CommandManager.argument("args", StringArgumentType.greedyString())
-                .executes(context -> {
-                    Identifier function = CommandFunctionArgumentType.getIdentifiedFunctions(context, "function").getFirst();
-                    // String function = StringArgumentType.getString(context, "function");
-                    String args = StringArgumentType.getString(context, "args");
-                    ServerCommandSource source = context.getSource();
-                    handleCommand(source, function, args);
-
-                    return Command.SINGLE_SUCCESS;
-                }))));
-    }
-
-    private void handleCommand(ServerCommandSource source, Identifier function, String args) {
-        String[] arguments = args.split(" (?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)");
-        StringBuilder jsonArgs = new StringBuilder("{");
-
-        for (int i = 0; i < arguments.length; i += 2) { // i += 1
-            arguments[i] = arguments[i].replaceAll("^\"|\"$", "");
-            arguments[i + 1] = arguments[i + 1].replaceAll("^\"|\"$", "");
-            if (i > 0) jsonArgs.append(", ");
-            jsonArgs.append("\"").append(arguments[i]).append("\":\"").append(arguments[i + 1]).append("\"");
-        }
-
-        jsonArgs.append("}");
-
-        String command = "/function " + function.toString() + " " + jsonArgs.toString();
-        source.getServer().getCommandManager().executeWithPrefix(source, command);
+                .executes(new CallCommand()))));
     }
 }
---
+++ src/main/java/create/convoy/simplefunctions/commands/CallCommand.java
@@ -0,0 +1,44 @@
+package create.convoy.simplefunctions.commands;
+
+import com.mojang.brigadier.Command;
+import com.mojang.brigadier.arguments.StringArgumentType;
+import com.mojang.brigadier.context.CommandContext;
+import com.mojang.brigadier.exceptions.CommandSyntaxException;
+
+import net.minecraft.command.argument.CommandFunctionArgumentType;
+import net.minecraft.server.command.ServerCommandSource;
+import net.minecraft.util.Identifier;
+
+public class CallCommand implements Command<ServerCommandSource> {
+    @Override
+    public int run(CommandContext<ServerCommandSource> context) throws CommandSyntaxException {
+        Identifier function = CommandFunctionArgumentType.getIdentifiedFunctions(context, "function").getFirst();
+        String args = StringArgumentType.getString(context, "args");
+        ServerCommandSource source = context.getSource();
+
+        String procArgs = buildArgs(args);
+
+        exec(source, function, procArgs);
+        return 0;
+    }
+
+    private String buildArgs(String args) {
+        String[] argsList = args.split(" (?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)");
+        StringBuilder jsonArgs = new StringBuilder("{");
+
+        for (int i = 0; i < argsList.length; i += 2) {
+            argsList[i] = argsList[i].replaceAll("^\"|\"$", "");
+            argsList[i + 1] = argsList[i + 1].replaceAll("^\"|\"$", "");
+            if (i > 0) jsonArgs.append(", ");
+            jsonArgs.append("\"").append(argsList[i]).append("\":\"").append(argsList[i + 1]).append("\"");
+        }
+
+        jsonArgs.append("}");
+        return jsonArgs.toString();
+    }
+
+    private void exec(ServerCommandSource source, Identifier function, String args) {
+        String command = "/function " + function.toString() + " " + args;
+        source.getServer().getCommandManager().executeWithPrefix(source, command);
+    }
+}

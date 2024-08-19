656120e2e511414b52a5dd9916ade70e600dfede
UnlikeDogVirtual3532 <kobe@roosemeyers.com>
2024-08-19 14:57:48 +0200

    Cleaned Up Code

--- src/main/java/create/convoy/simplefunctions/CommandRegistry.java
+++ src/main/java/create/convoy/simplefunctions/CommandRegistry.java
@@ -10,7 +10,7 @@ import net.minecraft.server.command.CommandManager;
 import net.minecraft.server.command.ServerCommandSource;
 
 public class CommandRegistry {
-    public static void register(CommandDispatcher<ServerCommandSource> dispatcher, boolean dedicated) {
+    public static void register(CommandDispatcher<ServerCommandSource> dispatcher) {
         dispatcher.register(CommandManager.literal("call")
                 .then(CommandManager.argument("function", CommandFunctionArgumentType.commandFunction()).suggests(Playercommands.SUGGESTION_PROVIDER)
                 .executes(new ShortCallCommand())
--- src/main/java/create/convoy/simplefunctions/Playercommands.java
+++ src/main/java/create/convoy/simplefunctions/Playercommands.java
@@ -1,19 +1,12 @@
 package create.convoy.simplefunctions;
 
-import com.mojang.brigadier.Command;
-import com.mojang.brigadier.CommandDispatcher;
-import com.mojang.brigadier.arguments.StringArgumentType;
 import com.mojang.brigadier.suggestion.SuggestionProvider;
 
-import create.convoy.simplefunctions.commands.CallCommand;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
-import net.minecraft.server.command.CommandManager;
 import net.minecraft.server.command.ServerCommandSource;
 import net.minecraft.server.function.CommandFunctionManager;
 import net.minecraft.command.CommandSource;
-import net.minecraft.command.argument.CommandFunctionArgumentType;
-import net.minecraft.util.Identifier;
 
 public class Playercommands implements ModInitializer {
 
@@ -26,22 +19,7 @@ public class Playercommands implements ModInitializer {
     @Override
     public void onInitialize() {
         CommandRegistrationCallback.EVENT.register((dispatcher, registryAcces, enviroment) -> {
-            CommandRegistry.register(dispatcher, false);
+            CommandRegistry.register(dispatcher);
         });
     }
-
-    // private void registerCommands(CommandDispatcher<ServerCommandSource> dispatcher, boolean dedicated) {
-    //     dispatcher.register(CommandManager.literal("call")
-    //             .then(CommandManager.argument("function", CommandFunctionArgumentType.commandFunction()).suggests(SUGGESTION_PROVIDER)
-    //             .executes(context -> {
-    //                 Identifier function = CommandFunctionArgumentType.getIdentifiedFunctions(context, "function").getFirst();
-    //                 ServerCommandSource source = context.getSource();
-
-    //                 String command = "/function " + function.toString();
-    //                 source.getServer().getCommandManager().executeWithPrefix(source, command);
-    //                 return Command.SINGLE_SUCCESS;
-    //             })
-    //             .then(CommandManager.argument("args", StringArgumentType.greedyString())
-    //             .executes(new CallCommand()))));
-    // }
 }

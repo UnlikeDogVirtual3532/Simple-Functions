d255bbbfdfacc91771a4945ccf7f6f625a5517bb
UnlikeDogVirtual3532 <kobe@roosemeyers.com>
2024-08-20 11:33:16 +0200

    Made a Config And Organized File Structure (again)

--- src/main/java/create/convoy/simplefunctions/Playercommands.java
+++ src/main/java/create/convoy/simplefunctions/Playercommands.java
@@ -1,7 +1,12 @@
 package create.convoy.simplefunctions;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import java.util.Map;
+
 import com.mojang.brigadier.suggestion.SuggestionProvider;
 
+import create.convoy.simplefunctions.config.ConfigManager;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
 import net.minecraft.server.command.ServerCommandSource;
@@ -9,6 +14,14 @@ import net.minecraft.server.function.CommandFunctionManager;
 import net.minecraft.command.CommandSource;
 
 public class Playercommands implements ModInitializer {
+    public static final String MOD_NAME = "Simple Functions";
+    public static final String MOD_ID = "simplefunctions";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+    public static String configFileLocation = "config/simplefunctions/";
+    public static String configFileName = "Simplefunctions.yaml";
+    public static String defaultConfigFilePath = "assets/simplefunctions/default_config.yaml";
+    public static Map<String, Object> configMap;
 
     public static final SuggestionProvider<ServerCommandSource> SUGGESTION_PROVIDER = (context, builder) -> {
 		CommandFunctionManager commandFunctionManager = context.getSource().getServer().getCommandFunctionManager();
@@ -18,6 +31,14 @@ public class Playercommands implements ModInitializer {
 
     @Override
     public void onInitialize() {
+        ConfigManager configManager = new ConfigManager(configFileLocation, configFileName, defaultConfigFilePath);
+
+        try {
+            configMap = configManager.getConfig();
+        } catch (Exception e) {
+            configMap = configManager.getDefaultConfig();
+        }
+
         CommandRegistrationCallback.EVENT.register((dispatcher, registryAcces, enviroment) -> {
             CommandRegistry.register(dispatcher);
         });
